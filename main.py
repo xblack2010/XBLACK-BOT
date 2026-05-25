10a13e0d435eb695c19d8cc20ba9d6fd42e6935b
Rassim BO$$ <zzzkarim000@gmail.com>
2026-05-25 15:53:42 -0700

    Update main.py

--- main.py
+++ main.py
@@ -2462,3 +2462,21 @@ async def StarTinG():
 
 if __name__ == '__main__':
     asyncio.run(StarTinG())
+
+
+
+
+from flask import Flask
+from threading import Thread
+
+app = Flask('')
+
+@app.route('/')
+def home():
+    return "Bot is running"
+
+def run():
+    app.run(host='0.0.0.0', port=8080)
+
+t = Thread(target=run)
+t.start()
